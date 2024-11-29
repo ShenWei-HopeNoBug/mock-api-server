5dce0c5d4d9953bddddb26fcbd190644cf312c1b
shenwei <sw18368337305@163.com>
2024-11-29 13:55:36 +0800

    抓包脚本改造

--- request_catch.py
+++ request_catch.py
@@ -8,20 +8,53 @@ from utils import create_md5, JsonFormat
 # 处理请求抓包工具类
 class RequestRecorder:
   def __init__(self):
-    self.response_map = {}
+    # 抓包数据保存路径
+    self.save_path = './output.xlsx'
+    # self.save_path = './test/output.xlsx'
 
+    # 抓包缓存数据 dict
+    self.response_catch_dict = {}
+
+  # 检查请求是否需要被抓取保存
+  @staticmethod
+  def check_response(request, response):
+    # 请求链接
+    url = request.url
+
+    # 需要排除的请求
+    except_reg = re.compile(r'\.(png|jpg|jpeg|gif|avif|webp|js|css|ico|ttf|html|xml)')
+    if except_reg.search(url):
+      return False
+
+    # 需要包含的请求
+    include_reg = re.compile(r'dream.aimiai.com/dream-plus')
+    if not include_reg.search(url):
+      return False
+
+    response_content_type = response.headers.get('Content-Type') or ''
+    # 忽略 json 以外的响应内容
+    if 'application/json' not in response_content_type:
+      return False
+
+    return True
+
+  # 保存抓包数据到缓存
   def save_response(self, record):
     secret_key = r'{}{}'.format(record['Method'], record['Params'])
     md5_key = create_md5(secret_key)
     search_key = r'{}{}'.format(record['Url'], record['Method'])
     # 创建新 url 的答案映射 dict
-    if search_key not in self.response_map:
-      self.response_map[search_key] = {}
+    if search_key not in self.response_catch_dict:
+      self.response_catch_dict[search_key] = {}
 
     # 添加新的请求 response 内容
-    self.response_map[search_key][md5_key] = record
+    self.response_catch_dict[search_key][md5_key] = record
 
   def response(self, flow: http.HTTPFlow):
+    # 请求检查
+    if not self.check_response(flow.request, flow.response):
+      return
+
     # 请求链接
     url = flow.request.url
 
@@ -34,14 +67,21 @@ class RequestRecorder:
     if not include_reg.search(url):
       return
 
+    response_content_type = flow.response.headers.get('Content-Type') or ''
+    # 忽略 json 以外的响应内容
+    if 'application/json' not in response_content_type:
+      return
+
     method = flow.request.method
 
     # 请求参数，统一用 json string
     params = JsonFormat.format_dict_to_json_string({})
+
+    request_content_type = flow.request.headers.get('content-type') or ''
     if method == 'POST':
-      if 'application/x-www-form-urlencoded' in flow.request.headers.get('content-type'):
+      if 'application/x-www-form-urlencoded' in request_content_type:
         params = flow.request.form
-      elif 'application/json' in flow.request.headers.get('content-type'):
+      elif 'application/json' in request_content_type:
         params = flow.request.get_text()
     elif method == 'GET':
       params = JsonFormat.format_dict_to_json_string(dict(flow.request.query.copy()))
@@ -66,7 +106,7 @@ class RequestRecorder:
     for name in fieldnames:
       data[name] = []
 
-    for response_data in self.response_map.values():
+    for response_data in self.response_catch_dict.values():
       for record in response_data.values():
         # 将数据存入对应列
         for key, value in record.items():
@@ -76,8 +116,8 @@ class RequestRecorder:
     df = pd.DataFrame(data)
 
     # 将DataFrame写入Excel文件，每行为一个数据
-    df.to_excel('output.xlsx', index=False, engine='openpyxl')
-    self.response_map = {}
+    df.to_excel(self.save_path, index=False, engine='openpyxl')
+    self.response_catch_dict = {}
 
 
 addons = [
