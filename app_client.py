a30cff11f6be8a63ef969fbd8c088c865ac54650
shenwei <sw18368337305@163.com>
2024-11-29 16:10:23 +0800

    先提交下qt代码

---
+++ app_client.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from PyQt5.QtGui import QCloseEvent
+from PyQt5.QtWidgets import (
+  QMainWindow,
+  QMessageBox,
+  QWidget,
+)
+
+
+# app 主窗口
+class MainWindow(QWidget):
+  def __init__(self):
+    super().__init__(None)
+    self.initUI()
+
+  # 初始化窗口 UI
+  def initUI(self):
+    self.setWindowTitle('mock server v0.0.1-bata')
+
+  def closeEvent(self, event: QCloseEvent):
+    reply = QMessageBox.question(
+      self,
+      '消息',
+      '确定要退出吗？',
+      QMessageBox.Yes | QMessageBox.No,
+      QMessageBox.No,
+    )
+
+    if reply == QMessageBox.Yes:
+      event.accept()
+    else:
+      event.ignore()
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
 from mock_server import MockServer
-
+from app_client import MainWindow
+from PyQt5.QtWidgets import QApplication
+import sys
 
 if __name__ == '__main__':
-  mock_server = MockServer()
+  # mock_server = MockServer()
 
   # 检查和下载静态资源
-  mock_server.check_static()
+  # mock_server.check_static()
 
   # 启本地 mock 服务
-  mock_server.start_server()
+  # mock_server.start_server()
+
+  app = QApplication(sys.argv)
+  main_window = MainWindow()
+  main_window.show()
+  sys.exit(app.exec_())
