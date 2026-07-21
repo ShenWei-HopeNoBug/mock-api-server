ea858867509cb045830f867a13f44826e6959867
shenwei <sw18368337305@163.com>
2026-07-21 20:57:42 +0800

    error_catch 支持无参数使用

--- lib/decorate.py
+++ lib/decorate.py
@@ -12,23 +12,30 @@ T = TypeVar('T')
 
 # 异常捕获
 def error_catch(
+    func: Optional[Callable[..., T]] = None,
+    *,
     error_msg: str = '',
     error_return: Any = None,
     log: bool = True,
-) -> Callable[[Callable[..., T]], Callable[..., T]]:
+) -> Union[Callable[..., T], Callable[[Callable[..., T]], Callable[..., T]]]:
   """
   异常捕获装饰器，捕获被装饰函数抛出的 Exception，返回 error_return 兜底值
 
+  支持两种用法：
+    @error_catch                                           # 无参，默认 error_return=None, log=True
+    @error_catch(error_msg='xxx', error_return=[], log=False)  # 带参
+
+  :param func: 被装饰函数（无参用法时由 Python 自动传入）
   :param error_msg: 自定义错误信息，为空时使用 'Error'
   :param error_return: 异常时返回的兜底值，会做 deepcopy
   :param log: 是否通过 APP_LOGGER 记录异常日志
   """
 
-  def decorate(func: Callable[..., T]) -> Callable[..., T]:
-    @wraps(func)
+  def _make_wrapper(_func: Callable[..., T]) -> Callable[..., T]:
+    @wraps(_func)
     def wrapper(*args: Any, **kwargs: Any) -> T:
       try:
-        return func(*args, **kwargs)
+        return _func(*args, **kwargs)
       except Exception as e:
         if log:
           message = error_msg or 'Error'
@@ -38,7 +45,12 @@ def error_catch(
 
     return wrapper
 
-  return decorate
+  # 无参用法：@error_catch
+  if func is not None:
+    return _make_wrapper(func)
+
+  # 带参用法：@error_catch(error_msg='xxx', ...)
+  return _make_wrapper
 
 
 # 线程装饰器
