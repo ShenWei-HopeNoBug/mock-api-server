a62b5719f70a0286a74a0e2de7e08333277873ab
shenwei <sw18368337305@163.com>
2026-07-29 19:51:14 +0800

    加上日志

--- tools/ico_transform.py
+++ tools/ico_transform.py
@@ -6,8 +6,23 @@ from PIL import Image
 def main():
   from_path = '../app-icon.png'
   save_path = '../assets/app.ico'
-  img = Image.open(from_path)
-  img.save(save_path, format="ICO", sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
+
+  import os
+  abs_from_path = os.path.abspath(from_path)
+  abs_save_path = os.path.abspath(save_path)
+
+  if not os.path.exists(abs_from_path):
+    print(f'[ERROR] 源文件不存在: {abs_from_path}')
+    sys.exit(1)
+  if not os.path.isfile(abs_from_path):
+    print(f'[ERROR] 源路径不是文件: {abs_from_path}')
+    sys.exit(1)
+
+  print(f'[INFO] 开始转换: {abs_from_path} -> {abs_save_path}')
+  img = Image.open(abs_from_path)
+  print(f'[INFO] 已打开图片: {abs_from_path}, 尺寸={img.size}')
+  img.save(abs_save_path, format="ICO", sizes=[(16, 16), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)])
+  print(f'[INFO] ICO 文件已保存: {abs_save_path}')
 
 
 if __name__ == '__main__':
